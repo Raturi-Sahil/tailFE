880bee9cd310a2d263ca435001b953d5453ddf10
Sahil Raturi <sahilraturi687@gmail.com>
2025-07-26 18:01:06 +0530

    All done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from './components/navbar'
 import Hero from './components/Hero'
 import Analytics from './components/Analytics'
 import NewsLetter from './components/NewsLetter'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+
 
 function App() {
 
@@ -13,6 +16,8 @@ function App() {
       <Hero />
       <Analytics />
       <NewsLetter />
+      <Cards />
+      <Footer />
     </>
   )
 }
